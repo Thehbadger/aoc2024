415821d05270935e4ff6bf1fa8e0414ecc36f940
Jacob Hooper <xboxjh@gmail.com>
2024-12-02 20:10:49 -0500

    d2 p1 done.

--- d2/d2.hs
+++ d2/d2.hs
@@ -4,7 +4,7 @@ main :: IO ()
 main = do
   input <- readFile "input"
   let reports = parse input
-  print input
+  print reports
 
 type Level = Int
 
@@ -19,24 +19,29 @@ data Direction
 
 getDirection :: Int -> Int -> (Direction, Int)
 getDirection x y
-  | x == y = (Hold, 0)
   | x > y = (Decreasing, x - y)
   | x < y = (Increasing, y - x)
+  | otherwise = (Hold, 0)
 
-parse :: String -> [Report]
-parse = map (map (read :: String -> Level) . words) . lines
+parse :: String -> Int
+parse = sum . map (fromEnum . (`isSafe` Unk) . map (read :: String -> Level) . words) . lines
 
 isSafe :: Report -> Direction -> Bool
 isSafe [x, xs] d -- Last two of the list
   | direction == Hold = False
-  | 
+  | (value <= 3) && (direction == d) = True
+  | otherwise = False
   where
     (direction, value) = getDirection x xs
-
-isSafe (x : xs : xxs) Unk
-
-isSafe (x : xs : xxs) d
+isSafe (x : xs : xxs) Unk -- Starting offfffffff
+  | value <= 3 = isSafe (xs : xxs) direction
+  | otherwise = False
+  where
+    (direction, value) = getDirection x xs
+isSafe (x : xs : xxs) d -- Normal Pattern
   | direction == Hold = False
-  | value < 3 = isSafe (xs : xxs) direction
+  | (value <= 3) && (d == direction) = isSafe (xs : xxs) direction
+  | otherwise = False
   where
     (direction, value) = getDirection x xs
+isSafe _ _ = False
